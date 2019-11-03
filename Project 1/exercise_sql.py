a18efb8114f2a7ca38ece99392cfb9dcc721b8ba
gnudrew <gnudrew@gmail.com>
2019-11-02 22:58:52 -0500

    lkhjlkj

---
+++ Project 1/exercise_sql.py
@@ -0,0 +1,21 @@
+import mysql.connector
+
+con = mysql.connector.connect(
+user = "ardit700_student",
+password = "ardit700_student",
+host = "108.167.140.122",
+database = "ardit700_pm1databasae",
+)
+
+cursor = con.cursor()
+
+word = input("Enter a word: ")
+
+query = cursor.execute("SELECT * FROM Dictionary WHERE Expression = '%s' " % word)
+results = cursor.fetchall()
+
+if results:
+    for result in results:
+        print(result[1])
+else:
+    print("No word found!")
