844b0816b37770acabb3aac96a2bc3197a23be0a
gnudrew <gnudrew@gmail.com>
2020-03-15 23:40:22 -0500

    adding backend

---
+++ Project 5/backend.py
@@ -0,0 +1,53 @@
+import sqlite3 as sql
+
+def connect():
+    conn=sql.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, isbn INTEGER)")
+    conn.commit()
+    conn.close()
+
+def insert(title, author, year, isbn):
+    conn=sql.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("INSERT INTO book VALUES (NULL,?,?,?,?)", (title, author, year, isbn))
+    conn.commit()
+    conn.close()
+
+def view():
+    conn=sql.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM book")
+    rows=cur.fetchall()
+    conn.close()
+    return rows
+
+def search(title="",author="",year="",isbn=""):
+    conn=sql.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?", (title,author,year,isbn))
+    rows=cur.fetchall()
+    conn.close()
+    return rows
+
+def delete(id):
+    conn=sql.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("DELETE FROM book WHERE id=?",(id,))
+    conn.commit()
+    conn.close()
+
+def update(id,title,author,year,isbn):
+    conn=sql.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("UPDATE book SET title=?, author=?, year=?, isbn=? WHERE id=?",(title,author,year,isbn,id))
+    conn.commit()
+    conn.close()
+   
+
+connect()
+# insert("The Sun","John Smith",1918,9819681651)
+# delete(2)
+update(4,"The Moon", "John Smooth", 1917, 999999)
+print(view())
+print(search(author="John Smith"))
