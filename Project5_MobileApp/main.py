881684c08e2d303831ea97f2a821d9a448a9500d
Andrew Reid <52947768+gnudrew@users.noreply.github.com>
2020-07-09 23:39:31 -0500

    implement Sign Up screen and Json File

--- Project5_MobileApp/main.py
+++ Project5_MobileApp/main.py
@@ -1,12 +1,23 @@
 from kivy.app import App 
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+import json
+from datetime import datetime
 
 Builder.load_file('design.kv')
 
 class LoginScreen(Screen):
     def sign_up(self):
-        print("sign up button pressed.")
+        self.manager.current = "sign_up_screen"
+
+class SignUpScreen(Screen):
+    def add_user(self, username, password):
+        with open("users.json") as file:
+            users = json.load(file)
+        users[username] = {'username' : username, 'password' : password, 
+            'created' : datetime.now().strftime("%Y-%m-%d %H-%M-%S")}
+        with open("users.json", 'w') as file:
+            json.dump(users, file)
 
 class RootWidget(ScreenManager):
     pass
