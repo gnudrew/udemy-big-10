052c25e00942515c710c88e5216703d8d23a8550
Andrew Reid <52947768+gnudrew@users.noreply.github.com>
2020-08-12 19:01:35 -0500

    finish funtionality and some styling

--- Project5_MobileApp/main.py
+++ Project5_MobileApp/main.py
@@ -1,8 +1,12 @@
 from kivy.app import App 
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-import json
+from hoverable import HoverBehavior
+from kivy.uix.image import Image
+from kivy.uix.behaviors import ButtonBehavior
+import json, glob, random
 from datetime import datetime
+from pathlib import Path
 
 Builder.load_file('design.kv')
 
@@ -10,8 +14,13 @@ class LoginScreen(Screen):
     def sign_up(self):
         self.manager.transition.direction = "right"
         self.manager.current = "sign_up_screen"
-    def login(self):
-        pass
+    def login(self, uname, pword):
+        with open("users.json") as file:
+            users = json.load(file)
+        if uname in users and users[uname]['password'] == pword:
+            self.manager.current = 'login_screen_success'
+        else:
+            self.ids.login_wrong.text = "Wrong username or password!"
 
 class SignUpScreen(Screen):
     def add_user(self, username, password):
@@ -34,7 +43,26 @@ class LoginScreenSuccess(Screen):
         self.manager.transition.direction = "right"
         self.manager.current = "login_screen"
 
+    def get_quote(self, feel):
+        feel = feel.lower()
+        available_feelings = glob.glob("quotes/*txt")
+
+        available_feelings = [
+            Path(filename).stem for filename in 
+            available_feelings
+        ]
+
+        if feel in available_feelings:
+            with open(f"quotes/{feel}.txt", encoding="utf-8") as file:
+                quotes = file.readlines()
+            self.ids.quote.text = random.choice(quotes)
+        else:
+            self.ids.quote.text = "Try another feeling..."
+
 class RootWidget(ScreenManager):
+    pass
+
+class ImageButton(ButtonBehavior, HoverBehavior, Image):
     pass
 
 class MainApp(App):
