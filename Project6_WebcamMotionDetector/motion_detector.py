84ce80c9c38d1073bf6136119e563daeb45e6ca2
gnudrew <gnudrew@gmail.com>
2020-08-26 10:52:04 -0500

    ..

--- Project6_WebcamMotionDetector/motion_detector.py
+++ Project6_WebcamMotionDetector/motion_detector.py
@@ -7,18 +7,18 @@ while True:
     check, frame = video.read()
 
     gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    gray=cv2.GaussianBlur(gray,(21,21),0)
+    gray2=cv2.GaussianBlur(gray,(21,21),0)
 
     if first_frame is None:
         first_frame=gray
         continue
     
-    delta_frame=cv2.absdiff(first_frame,gray)
+    delta_frame=cv2.absdiff(first_frame,gray2)
 
     thresh_delta=cv2.threshold(delta_frame,30, 255, cv2.THRESH_BINARY)[1]
 
     
-
+    cv2.imshow("Color", frame)
     cv2.imshow("Gray Frame",gray)
     cv2.imshow("Delta Frame",delta_frame)
     cv2.imshow("Threshold Frame",thresh_delta)
